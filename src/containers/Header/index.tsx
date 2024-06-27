841411177bc2ec5ded380e332333c009cf263776
Tallyson <tallyson.eu@outlook.com>
2024-06-27 08:18:10 -0300

    feat: remodule header

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-danger position-fixed top-0 start-0 w-100 z-2">
-            <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-light position-fixed top-0 start-0 w-100 z-2">
+            <div className="container-md p-1">
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -22,22 +22,22 @@ const Header = () => {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center">
                         <li className="nav-item">
                             <a
-                                className="nav-link active text-light"
+                                className="nav-link active text-danger fw-medium"
                                 aria-current="page"
                                 href="#"
                             >
-                                Products
+                                About
                             </a>
                         </li>
                         <li className="nav-item dropdown">
                             <a
-                                className="nav-link dropdown-toggle text-light"
+                                className="nav-link dropdown-toggle text-danger fw-medium"
                                 href="#"
                                 role="button"
                                 data-bs-toggle="dropdown"
                                 aria-expanded="false"
                             >
-                                Dropdown
+                                Produtos
                             </a>
                             <ul className="dropdown-menu">
                                 <li>
@@ -47,7 +47,7 @@ const Header = () => {
                                 </li>
                                 <li>
                                     <a className="dropdown-item" href="#">
-                                        Another action
+                                        Fragrancias
                                     </a>
                                 </li>
                                 <li>
@@ -55,13 +55,20 @@ const Header = () => {
                                 </li>
                                 <li>
                                     <a className="dropdown-item" href="#">
-                                        Something else here
+                                        Desodorantes
                                     </a>
                                 </li>
                             </ul>
                         </li>
                     </ul>
                 </div>
+                <button className="btn btn-danger position-relative me-4">
+                    <i className="bi bi-basket3" style={{ color: "#FFF" }}></i>
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        99+
+                        <span className="visually-hidden">unread messages</span>
+                    </span>
+                </button>
             </div>
         </nav>
     );
