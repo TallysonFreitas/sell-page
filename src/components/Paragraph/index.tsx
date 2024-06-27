a6e240e55bd0d7f61a93c673479abb3239ca3926
Tallyson <tallyson.eu@outlook.com>
2024-06-27 08:17:38 -0300

    feat: personalize Paragraph

--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 type ParagraphType = {
     size: "small" | "normal";
-    children: string;
+    children: string | React.ReactNode;
 };
 
 const Paragraph = ({ size, children }: ParagraphType) => {
     return (
-        <p className={size == "small" ? "lead fs-8 mb-0" : "lead fs-6 mb-0"}>
+        <p
+            className={
+                size == "small"
+                    ? "lead fs-8 mb-0 text-gray"
+                    : "lead fs-6 mb-0 text-gray fw-normal"
+            }
+        >
             {children}
         </p>
     );
