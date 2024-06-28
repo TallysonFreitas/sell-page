8b5f20141123b4278ea5661f5c4db2c896af5800
Tallyson <tallyson.eu@outlook.com>
2024-06-28 13:54:42 -0300

    feat: implement url search url

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import Header from "./containers/Header";
 import About from "./containers/About";
 
 function App() {
+    const product = new URLSearchParams(window.location.search);
+
     const [product_data, setProduct_data] = useState(null);
 
-    console.log(product_data);
-
     useEffect(() => {
-        fetch("http://127.0.0.1:5000/get-product/1")
-            .then((data) => data.json())
-            .then((res) => {
-                setTimeout(() => {
-                    setProduct_data(res);
-                }, 1000);
-            });
+        try {
+            fetch(
+                `http://127.0.0.1:5000/get-product/${product.get("product-id")}`
+            )
+                .then((data) => data.json())
+                .then((res) => {
+                    setTimeout(() => {
+                        setProduct_data(res);
+                    }, 2000);
+                });
+        } catch (error) {
+            console.log(error);
+        }
     }, []);
 
     return (
